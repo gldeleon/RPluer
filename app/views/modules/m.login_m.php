da6ac8d0571ed67a174c3b5da6ffb490b6572769
gldeleon <gldeleon@live.com.mx>
2015-12-16 12:58:15 -0600

    app2

---
+++ app/views/modules/m.login_m.php
@@ -0,0 +1,27 @@
+<div class="row">
+	<div class="alert-success"><center><h2>Error en usuario y contraseña, Favor de verificarlos</h2></center></div>
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Inicie sesión con su cuenta</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form" method="post" action="index.php">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Usuario" name="user" type="text" autofocus required="required">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Contraseña" name="contra" type="password" value="" required="required">
+                                </div>
+                                <div class="form-group">
+                                	<input class="form-control" placeholder="" name="sesion" type="hidden">
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <button class="btn btn-lg btn-success btn-block" type="submit">Iniciar sesión</a>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
